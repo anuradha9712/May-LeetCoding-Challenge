edd9d4c61c16872386c76a1cbb1cecce16811cb4
anuradha9712 <46045493+anuradha9712@users.noreply.github.com>
2020-05-06 14:54:34 +0530

    cpp code

---
+++ 6. Majority-Element.cpp
@@ -0,0 +1,44 @@
+# Majority Element
+
+'''
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+Example 1:
+
+Input: [3,2,3]
+Output: 3
+Example 2:
+
+Input: [2,2,1,1,1,2,2]
+Output: 2
+
+'''
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        //using Moore's Voting Algo
+        
+        int count = 1;
+        int major = nums[0];
+        for(int i=1;i<nums.size();i++){
+            
+             if(major == nums[i])
+                count++;
+             else
+                count--;
+           
+            if(count == 0){
+                major = nums[i];
+                count= 1;
+            }
+           
+        }
+        
+        return major;
+        
+    }
+};
